3efecffe418cfad723080233d9a209781f8bc515
jnns <jvajen@gmail.com>
2017-03-06 12:02:00 +0000

    Fix deployment issues.

--- import.sh
+++ import.sh
@@ -3,10 +3,19 @@
 # Configuration
 API_USER=system
 API_KEY=
+
 MSACCESS_DATABASE='mdb/Kurs.mdb'
 SQLITE_DATABASE='data.sqlite'
 
+TZ="Europe/Berlin"
+LANG="de_DE.UTF-8"
+DATE=`date +"%A %x %X Uhr"`
+
+
 # Do not touch the contents below unless you know what you're doing
+
+
+set -e  # abort immediately once a command exits with nonzero return value
 
 GREEN='\033[1;32m'
 NC='\033[0m'
@@ -25,19 +34,19 @@ for table in $( mdb-tables $MSACCESS_DATABASE ); do
 done
 
 echo -e "${GREEN}Creating schema${NC}"
-#sqlite3 $SQLITE_DATABASE < sql/schema.sql
+sqlite3 $SQLITE_DATABASE < sql/schema.sql
 
 echo -e "${GREEN}Importing table Bardienst${NC}"
-#sqlite3 $SQLITE_DATABASE < sql/Bardienst.sql
+sqlite3 $SQLITE_DATABASE < sql/Bardienst.sql
 
 echo -e "${GREEN}Importing table Mitarbeiter${NC}"
-#sqlite3 $SQLITE_DATABASE < sql/Mitarbeiter.sql
+sqlite3 $SQLITE_DATABASE < sql/Mitarbeiter.sql
 
 echo -e "${GREEN}Importing table Kurse${NC}"
-#sqlite3 $SQLITE_DATABASE < sql/Kurse.sql
+sqlite3 $SQLITE_DATABASE < sql/Kurse.sql
 
 echo -e "${GREEN}Importing table Ereignis${NC}"
-#sqlite3 $SQLITE_DATABASE < sql/Ereignis.sql
+sqlite3 $SQLITE_DATABASE < sql/Ereignis.sql
 
 
 echo -e "${GREEN}Generate output for Kurse${NC}"
@@ -62,21 +71,21 @@ sed '/\${table_content_kurse}/{r kurse.output.html
     d;};
     /\${table_content_bardienst}/{r bardienst.output.html
     d;}' template.html > $PAYLOAD
-rm $TMP_OUTPUT
+
+rm kurse.output.html
+rm bardienst.output.html
 
 # Add timestamp to payload
-sed -i "s/{{ timestamp }}/$(date)/g" $PAYLOAD
+sed -i "s@<!-- timestamp -->@$DATE@" $PAYLOAD
 
 # Update posting on Discourse
 POST_ID=81
-BODY=`cat $PAYLOAD`
 
 echo -e "${GREEN}Delivering payload${NC}"
-curl -X PUT \
-	-d "api_username=$API_USER" \
-	-d "api_key=$API_KEY" \
-	-d "post[raw]=$BODY" \
-	https://intern.ostbloc.de/posts/$POST_ID
+curl -X PUT -F api_username=$API_USER \
+  -F api_key=$API_KEY \
+  -F post[raw]="`cat $PAYLOAD`" \
+  https://intern.ostbloc.de/posts/$POST_ID
 
 echo -e "${GREEN}Cleaning up${NC}"
 rm $PAYLOAD
